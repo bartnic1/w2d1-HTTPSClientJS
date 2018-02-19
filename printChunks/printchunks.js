72bb6cb406ff11479cb4f8e9044169dc367fc8dc
Piotr <bartnic1@gmail.com>
2018-02-19 16:17:02 +0000

    Finished first data chunks exercise

--- printChunks/printchunks.js
+++ printChunks/printchunks.js
@@ -9,22 +9,34 @@ var requestOptions = {
   path: '/'
 };
 
-// notice that https.get takes a callback with one parameter -
-// response, which is a Stream that represents the HTTP response
-https.get(requestOptions, function (response) {
+function getAndPrintHTMLChunks () {
 
-  // set encoding of received data to UTF-8
-  response.setEncoding('utf8');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
 
-  // the callback is invoked when a `data` chunk is received
-  response.on('data', function (data) {
-    console.log('Chunk Received. Length:', data.length);
-  });
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
 
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log(data + '\n');
+    });
+
+  })
+}
+
+getAndPrintHTMLChunks();
+
+/*
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
   response.on('end', function() {
     console.log('Response stream complete.');
   });
-
-});
+*/
