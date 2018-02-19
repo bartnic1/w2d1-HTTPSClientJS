8912b23db32f120c5a67a01d6f1b835d04b04608
Piotr <bartnic1@gmail.com>
2018-02-19 16:36:59 +0000

    Finished step 3, invoking HTML body using request options

---
+++ HTMLWithOptions/htmloptions.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  var returnData = '';
+
+  https.get(options, function (response) {
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      returnData += data;
+    });
+
+    response.on('end', function() {
+      console.log(returnData);
+    });
+
+  })
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
